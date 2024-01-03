51c6f584ffa1db2532ed4293c32ba4df880af8c5
grumbletumbles <dimanaer123@gmail.com>
2024-01-03 21:51:08 +0300

    fixed: tests

--- SqlQuerierTests/SqlQuerierCreateTests.cs
+++ SqlQuerierTests/SqlQuerierCreateTests.cs
@@ -26,6 +26,14 @@ public class SqlQuerierCreateTests
     public void SqlQueirer_CreateTable_MultipleColumnTest()
     {
         var querier = new SqlQuerier.SqlQuerier();
+        var result = querier.Create().Table("test_table").Date("date").Decimal("decimal").Build();
+        Assert.Equal("CREATE TABLE test_table ( date DATE, decimal DECIMAL );", result);
+    }
+
+    [Fact]
+    public void SqlQueirer_CreateTable_ParameterColumnTest()
+    {
+        var querier = new SqlQuerier.SqlQuerier();
         var result = querier.Create().Table("test_table").BigInt("bigint", 1).Varchar("varchar", 239).Build();
         Assert.Equal("CREATE TABLE test_table ( bigint BIGINT(1), varchar VARCHAR(239) );", result);
     }
